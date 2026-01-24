c1559fe5a4135787d8b62b39307b0b4e7eae7fea
Gilad Tidhar <giladtidhar19@gmail.com>
2026-01-24 15:24:17 +0200

    add main page with static users

---
+++ app/src/main/java/com/example/students_app/StudentRepository.kt
@@ -0,0 +1,63 @@
+package com.example.students_app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.toString
+
+class Student (val picturePath: String, val name: String, val id: Int)  {
+    var check: Boolean = false
+
+    fun check() {
+        check = !check
+    }
+}
+object StudentRepository {
+    private val students = mutableListOf<Student>()
+
+    fun add(student: Student) {
+        students.add(student)
+    }
+    fun remove(student: Student){
+        students.remove(student)
+    }
+    fun getAll(): List<Student> {
+        return students
+    }
+    fun getById(id: Int): Student? {
+        return students.find { it.id == id }
+    }
+
+}
+
+class StudentAdapter(
+    private val students: List<Student>
+) : RecyclerView.Adapter<StudentAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.findViewById(R.id.textName)
+        val id: TextView = view.findViewById(R.id.textId)
+        val check: CheckBox = view.findViewById(R.id.checkbox)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.student_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val student = students[position]
+        holder.name.text = student.name
+        holder.id.text = student.id.toString()
+        holder.check.setOnClickListener {
+            student.check()
+            holder.check.isChecked = student.check
+        }
+    }
+
+    override fun getItemCount() = students.size
+}
